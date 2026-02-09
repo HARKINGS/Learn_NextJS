2da78d13fccb1f73a24b8b78521e4d714104263b
Harkins <tmngoc1108@gmail.com>
2026-02-09 14:47:52 +0700

    5.Design Modal Add new

--- src/components/app.table.tsx
+++ src/components/app.table.tsx
@@ -2,6 +2,8 @@
 
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
+import CreateModal from "./create.modal";
+import { useState } from "react";
 
 interface IProps {
     blogs: IBlog[];
@@ -9,37 +11,49 @@ interface IProps {
 
 function AppTable(props: IProps) {
     const { blogs } = props;
-    console.log(">>> check blogs in table:", blogs);
+    const [showModal, setShowModal] = useState<boolean>(false);
 
     return (
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Title</th>
-                    <th>Author</th>
-                    {/* <th>Content</th> */}
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                {blogs?.map((blog, index) => (
-                    <tr key={blog.id}>
-                        <td>{index + 1}</td>
-                        <td>{blog.title}</td>
-                        <td>{blog.author}</td>
-                        {/* <td>{blog.content}</td> */}
-                        <td>
-                            <Button variant="info">View</Button>
-                            <Button variant="primary" className="mx-3">
-                                Edit
-                            </Button>
-                            <Button variant="danger">Delete</Button>
-                        </td>
+        <>
+            <div
+                className="mb-3"
+                style={{ display: "flex", justifyContent: "space-between" }}
+            >
+                <h3>Table Blogs</h3>
+                <Button variant="success" onClick={() => setShowModal(true)}>
+                    Add New Blog
+                </Button>
+            </div>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Title</th>
+                        <th>Author</th>
+                        {/* <th>Content</th> */}
+                        <th>Action</th>
                     </tr>
-                ))}
-            </tbody>
-        </Table>
+                </thead>
+                <tbody>
+                    {blogs?.map((blog, index) => (
+                        <tr key={blog.id}>
+                            <td>{index + 1}</td>
+                            <td>{blog.title}</td>
+                            <td>{blog.author}</td>
+                            {/* <td>{blog.content}</td> */}
+                            <td>
+                                <Button variant="info">View</Button>
+                                <Button variant="primary" className="mx-3">
+                                    Edit
+                                </Button>
+                                <Button variant="danger">Delete</Button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+            <CreateModal showModal={showModal} setShowModal={setShowModal} />
+        </>
     );
 }
 
---
+++ src/components/create.modal.tsx
@@ -0,0 +1,87 @@
+"use-client";
+
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { useState } from "react";
+
+interface IProps {
+    showModal: boolean;
+    setShowModal: (show: boolean) => void;
+}
+
+function CreateModal(props: IProps) {
+    const { showModal, setShowModal } = props;
+
+    const [title, setTitle] = useState<string>("");
+    const [author, setAuthor] = useState<string>("");
+    const [content, setContent] = useState<string>("");
+
+    const handleSubmit = () => {
+        console.log(">>> submit:", { title, author, content });
+    };
+
+    const handleClose = () => {
+        setTitle("");
+        setAuthor("");
+        setContent("");
+        setShowModal(false);
+    };
+
+    return (
+        <>
+            <Modal
+                show={showModal}
+                onHide={() => handleClose()}
+                backdrop="static"
+                keyboard={false}
+                size="lg"
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Add a new blog</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter blog title"
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Author</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter blog author"
+                                value={author}
+                                onChange={(e) => setAuthor(e.target.value)}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Content</Form.Label>
+                            <Form.Control
+                                as="textarea"
+                                rows={3}
+                                value={content}
+                                onChange={(e) => setContent(e.target.value)}
+                            />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => handleClose()}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={() => handleSubmit()}>
+                        Save
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
+export default CreateModal;
