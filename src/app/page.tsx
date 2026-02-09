259f909db980276dc4c15d0767a533cf7eadf5cd
Harkins <tmngoc1108@gmail.com>
2026-02-09 11:11:05 +0700

    Call API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
@@ -5,8 +7,18 @@ import Link from "next/link";
 import appStyle from "@/styles/app.module.css";
 import harkinsStyle from "@/styles/harkins.module.css";
 import AppTable from "@/components/app.table";
+import { useEffect } from "react";
 
 export default function Home() {
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch("http://localhost:8000/blogs");
+            const data = await res.json();
+            console.log(">>> check res:", data);
+        };
+        fetchData();
+    }, []);
+
     return (
         <div>
             <ul>
